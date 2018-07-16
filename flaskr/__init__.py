a1c7e0b2acc95aa3683bdff23dc017e84d876a80
Aron <lishanggang1997@gmail.com>
2018-07-16 16:59:37 +0800

    create a identify blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -26,6 +26,8 @@ def create_app(test_config=None):
         return 'Hello, World!'
     from . import db
     db.init_app(app)
+    from . import auth
+    app.register_blueprint(auth.bp)
     return app
 
 
